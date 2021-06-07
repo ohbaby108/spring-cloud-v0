013c4f59cba5485bffead7b731d60adb14e033db
ohbaby108 <66946945+ohbaby108@users.noreply.github.com>
2021-06-07 11:26:33 +0800

    服务注册

---
+++ cloud-eureka-client-v0/src/main/java/com/xinrui/CloudEurekaClientV0Application.java
@@ -0,0 +1,13 @@
+package com.xinrui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaClientV0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaClientV0Application.class, args);
+    }
+
+}
